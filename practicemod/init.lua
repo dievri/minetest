a117515014259c0f47bf4fb541edc0c1164860ca
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-02 23:53:23 +0300

    add start position check

--- practicemod/init.lua
+++ practicemod/init.lua
@@ -1,6 +1,11 @@
 -- On world creating, create platform 10x10 starting from (1,0,1) to (10,0,10)
 -- c_dirt = minetest.get_content_id("default:cobble")
 minetest.register_on_generated(function(minp, maxp,  blockseed)
+	start_pos = {x=0, y=0, z=0}
+	if (minp.x <= start_pos.x and start_pos.x <= maxp.x
+	and minp.y <= start_pos.y and start_pos.y <= maxp.y
+	and minp.z <= start_pos.z and start_pos.z <= maxp.z)
+	then 
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local data = vm:get_data()
 	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
@@ -16,6 +21,7 @@ minetest.register_on_generated(function(minp, maxp,  blockseed)
 
 	vm:set_data(data)
 	vm:write_to_map()
+end
 end)
 
 -- On join player will be placed at (10, 10, 10)
