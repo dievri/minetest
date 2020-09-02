a63ef99e3b931931a77dc1f5873135cdca87a191
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-03 00:27:18 +0300

    refactor

--- practicemod/init.lua
+++ practicemod/init.lua
@@ -1,27 +1,25 @@
 -- On world creating, create platform 10x10 starting from (1,0,1) to (10,0,10)
--- c_dirt = minetest.get_content_id("default:cobble")
 minetest.register_on_generated(function(minp, maxp,  blockseed)
-	start_pos = {x=0, y=0, z=0}
+	start_pos = {x=0, y=0, z=0} -- starting position of platform
 	if (minp.x <= start_pos.x and start_pos.x <= maxp.x
-	and minp.y <= start_pos.y and start_pos.y <= maxp.y
-	and minp.z <= start_pos.z and start_pos.z <= maxp.z)
+		and minp.y <= start_pos.y and start_pos.y <= maxp.y
+		and minp.z <= start_pos.z and start_pos.z <= maxp.z)
 	then 
-	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-	local data = vm:get_data()
-	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
+		local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+		local data = vm:get_data()
+		local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
 	
-	for i in area:iter(
-		0, 0, 0,
-		10, 0, 10
-	) do
-	 
-		data[i] = {name="default:dirt"}
-		
+	-- iterave over starting and ending positions of platform
+	for i in area:iter(0, 0, 0,
+			  10, 0, 10) do
+		-- set node
+		data[i] = {name="default:dirt"}		
 	end
 
+	-- save data to map
 	vm:set_data(data)
-	vm:write_to_map()
-end
+	vm:write_to_map() 
+	end
 end)
 
 -- On join player will be placed at (10, 10, 10)
