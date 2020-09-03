ca4eccaa8eb2095c4c8438fc937dec5d0f2faff8
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-03 12:27:36 +0300

    add entity

--- practicemod/init.lua
+++ practicemod/init.lua
@@ -1,6 +1,23 @@
 -- On world creating, create platform 10x10 starting from (1,0,1) to (10,0,10)
+
+-- entity cube 
+minetest.register_entity("practicemod:simple", {
+	initial_properties = {
+	hp_max = 20,
+    physical = true,
+    weight = 5,
+    textures = { "default_stone.png", "default_stone.png", "default_stone.png", "default_stone.png", "default_stone.png", "default_stone.png" },
+    visual = "cube",
+    visual_size = {x=1, y=1},
+    spritediv = {x=1, y=1},
+    initial_sprite_basepos = {x=0, y=0},
+    is_visible = true,
+    makes_footstep_sound = false
+	} 
+})
+
 minetest.register_on_generated(function(minp, maxp,  blockseed)
-	start_pos = {x=0, y=0, z=0} -- starting position of platform
+local start_pos = {x=0, y=0, z=0} -- starting position of platform
 	if (minp.x <= start_pos.x and start_pos.x <= maxp.x
 		and minp.y <= start_pos.y and start_pos.y <= maxp.y
 		and minp.z <= start_pos.z and start_pos.z <= maxp.z)
@@ -13,7 +30,8 @@ minetest.register_on_generated(function(minp, maxp,  blockseed)
 	for i in area:iter(0, 0, 0,
 			  10, 0, 10) do
 		-- set node
-		data[i] = {name="default:dirt"}		
+		local c_glass= minetest.get_content_id("default:glass")
+		data[i] = c_glass
 	end
 
 	-- save data to map
@@ -26,4 +44,6 @@ end)
 minetest.register_on_joinplayer(function(player)
 	minetest.log("action", "hello world")
 	player:set_pos({ x = 10, y = 10, z = 10})
+	minetest.add_entity({x = 5, y = 5, z = 15}, "practicemod:simple") 
 end)
+	
